c6a8637d6be1ff485f842f0467d2d3e222f035cb
dexterdc <sebasdc9@hotmail.com>
2024-06-26 18:02:14 -0600

    ending the menubar

--- src/components/MenuLogo.tsx
+++ src/components/MenuLogo.tsx
@@ -6,9 +6,49 @@ const bebas = Bebas_Neue({
 });
 
 function MenuLogo() {
+  const menuoptions = [
+    {
+      id: 1,
+      option: "Home",
+      src: "/",
+    },
+    {
+      id: 2,
+      option: "Catálogo",
+      src: "/catalog",
+    },
+    {
+      id: 3,
+      option: "Colección",
+      src: "/collection",
+    },
+    {
+      id: 4,
+      option: "Ventas",
+      src: "/sales",
+    },
+    {
+      id: 5,
+      option: "Blog",
+      src: "/blog",
+    },
+    {
+      id: 6,
+      option: "Sobre nosotros",
+      src: "/about",
+    },
+    {
+      id: 7,
+      option: "Contáctanos",
+      src: "/contact",
+    },
+  ];
+
   return (
     <section
-      className={"min-h-60 max-h-72 flex flex-col items-center justify-evenly"}
+      className={
+        "min-h-60 max-h-72 flex flex-col items-center justify-evenly gap-2 p-1"
+      }
     >
       <article
         className={`${bebas.className} mt-1 leading-10 tracking-wider text-4xl flex flex-col items-center justify-center`}
@@ -18,29 +58,21 @@ function MenuLogo() {
         </a>
         <span className="-mt-3">Ropavejero</span>
       </article>
-      <article className="w-full max-w-screen-lg font-semibold">
-        <ul className="flex p-2 justify-between ">
-          <a href="">
-            <li>Home</li>
-          </a>
-          <a href="">
-            <li>Catálogo</li>
-          </a>
-          <a href="">
-            <li>Colección</li>
-          </a>
-          <a href="">
-            <li>Ventas</li>
-          </a>
-          <a href="">
-            <li>Blog</li>
-          </a>
-          <a href="">
-            <li>Sobre nosotros</li>
-          </a>
-          <a href="">
-            <li>Contáctanos</li>
-          </a>
+      <article
+        className={`w-full max-w-screen-lg font-semibold tracking-wider text-xl ${bebas.className}`}
+      >
+        <ul className="flex p-2 justify-between uppercase">
+          {menuoptions.map((option) => {
+            return (
+              <a
+                href={option.src}
+                key={option.id}
+                className="hover:text-gray-600"
+              >
+                <li>{option.option}</li>
+              </a>
+            );
+          })}
         </ul>
       </article>
     </section>
