53e94f6815d75aca4e9b6abe9f061b75217aa558
DexterDC <sebasdc9@hotmail.com>
2024-07-12 18:40:43 -0600

    added banner

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,6 +1,30 @@
+import { useState, useRef } from "react";
 function Banner() {
+  const bannerImages = [
+    {
+      src: "/banner/1.webp",
+      alt: "banner1",
+    },
+    {
+      src: "/banner/2.webp",
+      alt: "banner2",
+    },
+    {
+      src: "/banner/3.webp",
+      alt: "banner3",
+    },
+  ];
+
   return (
     <section className="container mx-auto m-8 flex relative border h-[35rem]">
+      {bannerImages.map((image, index) => (
+        <img
+          key={index}
+          src={image.src}
+          alt={image.alt}
+          className="w-full h-full object-cover absolute overflow-hidden"
+        />
+      ))}
       <button
         className="bg-black text-white border rounded-md px-4 py-2 font-semibold absolute 
       top-64 left-0 m-4"
