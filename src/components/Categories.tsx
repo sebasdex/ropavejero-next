c717a127821222fee29c3ef66444a18209671c27
dexterdc <sebasdc9@hotmail.com>
2024-07-18 12:00:53 -0600

    add banner

--- src/components/Categories.tsx
+++ src/components/Categories.tsx
@@ -1,11 +1,24 @@
+import Link from "next/link";
 import React from "react";
 
 function Categories() {
   return (
-    <section className="flex flex-wrap justify-center gap-4 p-4 border">
-      <article className="h-60 w-60 border-2 border-black"></article>
-      <article className="h-60 w-60 border-2 border-black"></article>
-      <article className="h-60 w-60 border-2 border-black"></article>
+    <section className="flex flex-wrap justify-center gap-4 p-4 font-bold uppercase text-white text-2xl">
+      <article className="h-60 relative w-60 border border-black flex flex-col items-center justify-center bg-menGrid bg-cover bg-center before:block before:absolute before:-inset-0 before:bg-black before:opacity-20">
+        <span className="relative">
+          <Link href={"/men"}>Men</Link>
+        </span>
+      </article>
+      <article className="h-60 relative w-60 border border-black flex flex-col items-center justify-center bg-phrase bg-cover bg-bottom before:block before:absolute before:-inset-0 before:bg-black before:opacity-20">
+        <span className="relative">
+          <Link href={"/women"}>Women</Link>
+        </span>
+      </article>
+      <article className="h-60 relative w-60 border border-black flex flex-col items-center justify-center bg-teens bg-cover bg-center before:block before:absolute before:-inset-0 before:bg-black before:opacity-20">
+        <span className="relative">
+          <Link href={"/teens"}>Teens</Link>
+        </span>
+      </article>
     </section>
   );
 }
