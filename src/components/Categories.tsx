c8255bf876bff1c4d990f5eecc98c6a29299c282
dexterdc <sebasdc9@hotmail.com>
2024-07-20 22:56:13 -0600

    added hover effect

--- src/components/Categories.tsx
+++ src/components/Categories.tsx
@@ -4,17 +4,17 @@ import React from "react";
 function Categories() {
   return (
     <section className="flex flex-wrap justify-center gap-4 p-4 font-bold uppercase text-white text-2xl">
-      <article className="h-60 relative w-60 border border-black flex flex-col items-center justify-center bg-menGrid bg-cover bg-center before:block before:absolute before:-inset-0 before:bg-black before:opacity-20">
+      <article className="h-60 relative w-60 flex flex-col items-center justify-center bg-menGrid bg-cover bg-center before:block before:absolute before:-inset-0 before:bg-black before:opacity-20 hover:before:opacity-30">
         <span className="relative">
           <Link href={"/men"}>Men</Link>
         </span>
       </article>
-      <article className="h-60 relative w-60 border border-black flex flex-col items-center justify-center bg-phrase bg-cover bg-bottom before:block before:absolute before:-inset-0 before:bg-black before:opacity-20">
+      <article className="h-60 relative w-60  flex flex-col items-center justify-center bg-phrase bg-cover bg-bottom before:block before:absolute before:-inset-0 before:bg-black before:opacity-20 hover:before:opacity-30">
         <span className="relative">
           <Link href={"/women"}>Women</Link>
         </span>
       </article>
-      <article className="h-60 relative w-60 border border-black flex flex-col items-center justify-center bg-teens bg-cover bg-center before:block before:absolute before:-inset-0 before:bg-black before:opacity-20">
+      <article className="h-60 relative w-60  flex flex-col items-center justify-center bg-teens bg-cover bg-center before:block before:absolute before:-inset-0 before:bg-black before:opacity-20 hover:before:opacity-30">
         <span className="relative">
           <Link href={"/teens"}>Teens</Link>
         </span>
