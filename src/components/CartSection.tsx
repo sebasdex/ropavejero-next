825aa8dcb12dda0884169c542ab99e11d0d7e0e4
dexterdc <sebasdc9@hotmail.com>
2024-08-23 20:01:01 -0600

    update cart

--- src/components/CartSection.tsx
+++ src/components/CartSection.tsx
@@ -38,7 +38,7 @@ function CartSection() {
             </p>
           </span>
           <button className="bg-black text-white  px-4 py-2 text-sm font-semibold hover:bg-gray-300">
-            Checkout
+            Comprar
           </button>
         </footer>
       )}
--- src/components/Trend.tsx
+++ src/components/Trend.tsx
@@ -1,9 +1,11 @@
 import { dbShirts } from "@/db/db";
 import { useState } from "react";
 import Image from "next/image";
+import useStore from "@/store/myState";
 
 function Trend() {
   const [trendShirt, setTrendShirt] = useState(dbShirts);
+  const { addToCart } = useStore();
 
   return (
     <article className="flex flex-wrap justify-center gap-4 w-full mb-12">
@@ -23,7 +25,10 @@ function Trend() {
               />
               <h3 className="pt-2">{shirt.name}</h3>
               <p className="py-2 font-semibold">${shirt.price}</p>
-              <button className="bg-black text-white p-2 hover:bg-gray-600 ease-in-out duration-300">
+              <button
+                className="bg-black text-white p-2 hover:bg-gray-600 ease-in-out duration-300"
+                onClick={() => addToCart(shirt)}
+              >
                 Añadir al carrito
               </button>
             </div>
