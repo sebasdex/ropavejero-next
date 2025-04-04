e5ef3414f06c247c5634e18b97b239991da069f8
sebasdex <sebasdc9@hotmail.com>
2025-04-04 14:24:56 -0600

    feat: Enhance Footer component styling for improved readability and user engagement

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,26 +4,30 @@ import React from "react";
 function Footer() {
   return (
     <footer className="bg-black text-white w-full h-20 flex flex-col justify-center items-center text-sm">
-      <p>
-        Desarrollado por{" "}
-        <Link
-          href="https://github.com/sebasdex"
-          target="_blank"
-          rel="noreferrer"
-        >
-          SebastianDC
-        </Link>
-      </p>
-      <p>
-        Basado en el diseño de{" "}
-        <Link
-          href="https://theme528-t-shirt.myshopify.com/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <q>Urban Tee</q>
-        </Link>
-      </p>
+      <div className="flex flex-col items-center space-y-2">
+        <p className="text-sm font-medium tracking-wide">
+          Desarrollado por{" "}
+          <Link
+            href="https://github.com/sebasdex"
+            target="_blank"
+            rel="noreferrer"
+            className="font-semibold text-white hover:text-white/80 transition-colors duration-300"
+          >
+            SebastianDC
+          </Link>
+        </p>
+        <p className="text-sm font-medium tracking-wide">
+          Basado en el diseño de{" "}
+          <Link
+            href="https://theme528-t-shirt.myshopify.com/"
+            target="_blank"
+            rel="noreferrer"
+            className="font-semibold text-white hover:text-white/80 transition-colors duration-300 italic"
+          >
+            <q>Urban Tee</q>
+          </Link>
+        </p>
+      </div>
     </footer>
   );
 }
