a78489e5a812743afa370984b76db8d10673b6bd
sebasdex <sebasdc9@hotmail.com>
2025-04-04 11:49:02 -0600

    feat: Enhance Title component styling for improved visual appeal and user engagement

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -1,7 +1,8 @@
 function Title({ text }: { text: string }) {
   return (
-    <span className="text-center p-8 uppercase font-semibold tracking-wider">
-      {text}
+    <span className="block my-8 text-center p-8 uppercase font-semibold tracking-widest text-gray-900 bg-gradient-to-b from-white to-gray-100 rounded-lg shadow-md hover:shadow-lg transition-all duration-300 relative overflow-hidden group">
+      <span className="relative z-10">{text}</span>
+      <div className="absolute top-0 left-1/2 transform -translate-x-1/2 w-16 h-1 bg-gray-900 rounded-b-md"></div>
     </span>
   );
 }
